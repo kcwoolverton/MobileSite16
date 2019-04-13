aff2b85a73bfdeacff845418f73649d6101edbfa
kcwoolverton <kcwoolverton@gmail.com>
2019-04-13 16:33:38 -0700

    did first doctor room

--- TextBasedAdventureGame/app/src/main/java/com/example/textbasedadventuregame/FoundationEmployeeLivingQuartersDr1Room.java
+++ TextBasedAdventureGame/app/src/main/java/com/example/textbasedadventuregame/FoundationEmployeeLivingQuartersDr1Room.java
@@ -2,13 +2,67 @@ package com.example.textbasedadventuregame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class FoundationEmployeeLivingQuartersDr1Room extends AppCompatActivity {
+    public static AppDatabase DBINSTANCE;
+
+    public void onNextFoundationEmployeesDr1ButtonClick(View view) {
+        RadioGroup group = (RadioGroup) findViewById(R.id.foundation_employees_dr1_option_group);
+        int checkedButtonId = group.getCheckedRadioButtonId();
+        if (checkedButtonId == -1) {
+            Context context = getApplicationContext();
+            CharSequence text = "What would you like to do?";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        } else {
+            RadioButton selectedButton = findViewById(checkedButtonId);
+            int position = group.indexOfChild(selectedButton);
+            if (position == 0) {
+                // They chose to search through the closet
+                TextView text = (TextView) findViewById(R.id.additional_quarters_dr1_text);
+                text.setText("closet");
+            } else if (position == 1) {
+                // They chose to go through the bathroom
+                TextView text = (TextView) findViewById(R.id.additional_quarters_dr1_text);
+                text.setText("bathroom");
+            } else if (position == 2) {
+                // They chose to go through the desk
+                TextView text = (TextView) findViewById(R.id.additional_quarters_dr1_text);
+                text.setText("desk");
+            } else if (position == 3) {
+                // They chose to look at the posters
+                TextView text = (TextView) findViewById(R.id.additional_quarters_dr1_text);
+                text.setText("posters");
+            } else if (position == 4) {
+                // They chose to return to the employee quarters
+                Intent quartersIntent = new Intent(this, FoundationEmployeeLivingQuarters.class);
+
+                // Start the new activity.
+                startActivity(quartersIntent);
+            } else {
+                // panic?
+                TextView text = (TextView) findViewById(R.id.additional_quarters_dr1_text);
+                text.setText("panic?");
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_foundation_employee_living_quarters_dr1_room);
+        setContentView(R.layout.activity_foundation_employee_living_quarters_armory);
+        DBINSTANCE = AppDatabase.getDatabase(getApplicationContext());
     }
 }
